0c3651979a483f4de793b3235e3927bd44628dd0
cxfksword <718792+cxfksword@users.noreply.github.com>
2025-11-13 13:48:52 +0800

    ci: fix build error

--- Sources/BuildScripts/XCFrameworkBuild/main.swift
+++ Sources/BuildScripts/XCFrameworkBuild/main.swift
@@ -4,38 +4,38 @@ do {
     let options = try ArgumentOptions.parse(CommandLine.arguments)
     try Build.performCommand(options)
 
-    // // SSL
-    // try BuildOpenSSL().buildALL()
-    // try BuildGmp().buildALL()
-    // try BuildNettle().buildALL()
-    // try BuildGnutls().buildALL()
+    // SSL
+    try BuildOpenSSL().buildALL()
+    try BuildGmp().buildALL()
+    try BuildNettle().buildALL()
+    try BuildGnutls().buildALL()
 
-    // // libass
-    // try BuildUnibreak().buildALL()
-    // try BuildFreetype().buildALL()
-    // try BuildFribidi().buildALL()
-    // try BuildHarfbuzz().buildALL()
-    // try BuildASS().buildALL()
+    // libass
+    try BuildUnibreak().buildALL()
+    try BuildFreetype().buildALL()
+    try BuildFribidi().buildALL()
+    try BuildHarfbuzz().buildALL()
+    try BuildASS().buildALL()
 
-    // // libsmbclient
-    // try BuildSmbclient().buildALL()
+    // libsmbclient
+    try BuildSmbclient().buildALL()
 
-    // // libbluray
-    // try BuildBluray().buildALL()
+    // libbluray
+    try BuildBluray().buildALL()
     
-    // // ffmpeg
-    // try BuildUavs3d().buildALL()
-    // try BuildDovi().buildALL()
-    // try BuildVulkan().buildALL()
-    // try BuildShaderc().buildALL()
-    // try BuildLittleCms().buildALL()
-    // try BuildPlacebo().buildALL()
-    // try BuildDav1d().buildALL()
+    // ffmpeg
+    try BuildUavs3d().buildALL()
+    try BuildDovi().buildALL()
+    try BuildVulkan().buildALL()
+    try BuildShaderc().buildALL()
+    try BuildLittleCms().buildALL()
+    try BuildPlacebo().buildALL()
+    try BuildDav1d().buildALL()
     try BuildFFMPEG().buildALL()
 
-    // // mpv
-    // try BuildUchardet().buildALL()
-    // try BuildLuaJIT().buildALL()
+    // mpv
+    try BuildUchardet().buildALL()
+    try BuildLuaJIT().buildALL()
     try BuildMPV().buildALL()
 } catch {
     print(error.localizedDescription)
